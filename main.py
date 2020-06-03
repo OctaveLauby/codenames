3b6a6fe97066aaa666949655a5dfbee765f266f5
OctaveLauby <octavelauby@gmail.com>
2020-06-03 16:13:28 +0200

    renaming & code splitting

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pygame as pg
-from os.path import join
+from os.path import dirname, join
 from oldisplay import Window
 from oldisplay.components import Grid, Rectangle, Text
-from olutils import load, read_params
+from olutils import load, mkdirs, read_params, wait_until
 from logzero import logger
 
 
@@ -18,10 +18,8 @@ GRID_PARAMS = {
     'bot_txt_font': None,
     'bot_txt_c': 'dodger_blue',
     'bot_txt_rect': 'lavender',
-    'back_c': 'white',
     'line_c': 'light_grey',
 }
-
 
 
 def load_words(dictionary):
@@ -33,16 +31,37 @@ def load_words(dictionary):
     logger.info(f"{len(words)} words loaded for dictionary '{dictionary}'")
     return words
 
+def screenshot(window, output_path):
+    """Screenshot window"""
+    tick = window.ticks
+    wait_until(lambda: window.ticks > tick, timeout=5 * window.settings.fps)
+    path = join(OUTPUT_DIR, output_path)
+    mkdirs(dirname(path))
+    pg.image.save(window.screen, path)
 
-def create_word_grid(words, page_size, card_size, save=None, show=None, **params):
-    """Create grid of words"""
+
+def display_grid(window, card_size, color):
+    page_dx, page_dy = window.settings.size
+    card_dx, card_dy = card_size
+    assert page_dx > card_dx, "Required card x-size must be smaller than page"
+    assert page_dy > card_dy, "Required card y-size must be smaller than page"
+
+    kwargs = {'color': color}
+    window.components.append(Grid(card_dx, card_dy, **kwargs))
+    grid_di = page_dy // card_dy
+    grid_dj = page_dx // card_dx
+    cell_nb = grid_di * grid_dj
+    logger.info(f"Grid built with {grid_di}x{grid_dj}={cell_nb} cells")
+    return grid_di, grid_dj
+
+def display_word_cards(window, words, card_size, **params):
+    """Display one grid of words on window"""
+    assert window.initiated, "Window must be initialized"
     params = read_params(params, GRID_PARAMS)
+    window.components = []
 
     # ---- Read parameters
-    show = True if save is None and show is None else show
-    assert save or show, "Create grid requires to at least save or show result"
-
-    page_dx, page_dy = page_size
+    page_dx, page_dy = window.settings.size
     card_dx, card_dy = card_size
     assert page_dx > card_dx, "Required card x-size must be smaller than page"
     assert page_dy > card_dy, "Required card y-size must be smaller than page"
@@ -53,17 +72,9 @@ def create_word_grid(words, page_size, card_size, save=None, show=None, **params
     bot_txt_font = params.top_txt_font if params.bot_txt_font is None else params.bot_txt_font
     bot_txt_c = params.top_txt_c if params.bot_txt_c is None else params.bot_txt_c
 
-
-    # ---- Initiate Page
-    window = Window(size=page_size, background=params.back_c)
-
     # ---- Create Grid
-    kwargs = {'color': params.line_c}
-    window.components.append(Grid(card_dx, card_dy, **kwargs))
-    grid_di = page_dy // card_dy
-    grid_dj = page_dx // card_dx
+    grid_di, grid_dj = display_grid(window, card_size, color=params.line_c)
     cell_nb = grid_di * grid_dj
-    logger.info(f"Grid built with {grid_di}x{grid_dj}={cell_nb} cells")
 
     # ---- Write Words
     if len(words) > cell_nb:
@@ -104,19 +115,27 @@ def create_word_grid(words, page_size, card_size, save=None, show=None, **params
         ]
 
 
-    # ---- Return
-    window.open()
-    if save:
-        pg.image.save(window.screen, join(OUTPUT_DIR, save))
-    if show:
-        window.wait_close()
-    else:
-        window.stop = True
+def create_word_cards(window, words, card_size, directory, **params):
+    """Create grids of word cards"""
+    page_dx, page_dy = page_size
+    card_dx, card_dy = card_size
+    grid_di = page_dy // card_dy
+    grid_dj = page_dx // card_dx
+    cell_nb = grid_di * grid_dj
+
+    path_frmt = join(directory, "word_cards_{:02d}.jpg")
+    for count, i in enumerate(range(0, len(words), cell_nb), 1):
+        display_word_cards(
+            window,
+            words[i:i+cell_nb],
+            card_size,
+            **params
+        )
+        screenshot(window, path_frmt.format(count))
 
 
-def create_word_grids(words, page_size, card_size, output, **params):
+def create_validation_cards(window, card_size, ):
     raise NotImplementedError
-
 
 
 if __name__ == "__main__":
@@ -128,13 +147,24 @@ if __name__ == "__main__":
     card = (4, 2.5)
     factor = 45
 
-    # Code
+    # Read Params
     page_size = int(page[0] * factor), int(page[1] * factor)
     card_size = int(card[0] * factor), int(card[1] * factor)
+
+
+    # Initialize components
+    window = Window(size=page_size, background='white')
+    window.open()
     words = load_words(dictionary)
     rd.shuffle(words)
-    create_word_grid(
-        words, page_size, card_size,
-        save="word_grid.jpg",
-        show=True,
+
+    # Build word grid
+    create_word_cards(
+        window,
+        words,
+        card_size,
+        directory=f"output_{dictionary}"
     )
+
+    # End
+    window.wait_close()
